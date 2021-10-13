5755283d7fd03e9d6aa5cff6d144c2b38e679d5e
eomeuije <euije@gmail.com>
2021-10-13 20:46:34 +0900

    add gacha command & gacha data

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,6 +7,14 @@ const commands = [
 	new SlashCommandBuilder().setName('무야호').setDescription('무야호를 외칩니다.'),
 	new SlashCommandBuilder().setName('서버').setDescription('서버 정보를 알려줍니다.'),
 	new SlashCommandBuilder().setName('내정보').setDescription('내 디코 정보를 알려줍니다.'),
+	new SlashCommandBuilder().setName('가챠').setDescription('가챠')
+	.addIntegerOption(option => option.setName('종류').setDescription(`어떤 가챠를 하시겠습니까?
+1 = 이건 못 참지 상자
+2 = 알파몬x왕룡검
+3 = 미네르바몬x`))
+	.addIntegerOption(option => option.setName('개수').setDescription('가챠 개수를 알려주세요.(1번은 최대 50)')),
+
+	new SlashCommandBuilder().setName('ping').setDescription('is ping command.')
 ]
 	.map(command => command.toJSON());
 	
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Require the necessary discord.js classes
+const { randomInt } = require('crypto');
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
@@ -11,11 +12,13 @@ client.once('ready', () => {
 	client.user.setActivity('/(명령어)', { type: 'PLAYING' })
 });
 
+const wait = require('util').promisify(setTimeout);
+
 client.on('interactionCreate', async interaction => {
+	//console.log(interaction);
 	if (!interaction.isCommand()) return;
 
 	const { commandName } = interaction;
-	console.log(commandName);
 
 	if (commandName === '무야호') {
 		await interaction.reply(`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣴⣾⣿⣶⣶⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
@@ -43,6 +46,108 @@ client.on('interactionCreate', async interaction => {
 태그: ${interaction.user.tag}
 서버 참여 날짜: ${interaction.member.joinedAt}
 계정 생성 날짜: ${interaction.user.createdAt}`);
+	}else if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+		await wait(2000);
+		await interaction.editReply('Pong again!');
+	}else if (commandName === '가챠') {
+		const what = interaction.options.getInteger('종류');
+		const count = interaction.options.getInteger('개수');
+		if(!what | !count)
+		{
+			await interaction.reply(`종류나 개수가 입력되지 않았습니다. 탭이나 방향키를 이용해 입력해 주십시오.`);	
+			return;
+		}
+		if(what == 1 && count > 50)
+		{
+			await interaction.reply(`1번 종류의 개수는 50개까지 허용합니다.`);	
+			return;
+		}
+		
+		var gacha = require('./data/gacha.json');
+		var gachadet = gacha[`${what}`]
+		if(!gachadet)
+		{
+			await interaction.reply(`해당 종류의 가챠가 없습니다.`);	
+			return;
+		}
+		var gachastr = [];
+		var gacharesult = [];
+		var objcount = Object.keys(gachadet).length;
+		for(var i=0;i<objcount;i++)
+			gacharesult.push(0);
+		if(what == 1){
+			for(var i=0;i<count;i++)
+			{
+				var o;
+				var j;
+				var st = parseInt(Math.random() * 10000);
+				if(st < 100*(j=0.5))
+					o = 1;
+				else if(st < 100*(j+=0.5))
+					o = 2;
+				else if(st < 100*(j+=5))
+					o = 3;
+				else if(st < 100*(j+=9))
+					o = 4;
+				else if(st < 100*(j+=15))
+					o = 5;
+				else if(st < 100*(j+=17))
+					o = 6;
+				else if(st < 100*(j+=17))
+					o = 7;
+				else if(st < 100*(j+=18))
+					o = 8;
+				else if(st < 100*(j+=18))
+					o = 9;
+				gachastr[i] = `${i+1}번째: < ${gachadet[o]} > `
+				gacharesult[o - 1] += 1;
+			}
+		}
+		if(what == 2){	// x항체는 결과만 출력
+			for(var i=0;i<count;i++)
+			{
+				var o;
+				var j;
+				var st = parseInt(Math.random() * 10000);
+				if(st < 100*(j=0.1))
+					o = 1;
+				else if(st < 100*(j+=99.9))
+					o = 2;
+				gacharesult[o - 1] += 1;
+			}
+		}
+		if(what == 3){	// x항체는 결과만 출력
+			for(var i=0;i<count;i++)
+			{
+				var o;
+				var j;
+				var st = parseInt(Math.random() * 10000);
+				if(st < 100*(j=0.75))
+					o = 1;
+				else if(st < 100*(j+=99.25))
+					o = 2;
+				gacharesult[o - 1] += 1;
+			}
+		}
+		var str = "";
+		for(var i=0;i<gachastr.length;i++)
+		{
+			if(i != 0 && i % 2 == 0)
+				str += '\n';
+				
+			str += gachastr[i];
+			for(var s=gachastr[i].length;s<27;s++)
+				if(i % 2 == 0)
+					str += '　';
+		}
+		str += '\n\n\n결과:\n';
+
+		for(var i=0;i<objcount;i++)
+		{
+			str += `${gachadet[i+1]}	${gacharesult[i]}개\n`
+		}
+		await interaction.reply(str);
 	}
 });
 
